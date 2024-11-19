5193a5e483f16dd8fae4a9482a916420519b1b70
Max <max.zimmer@sky.uk>
2024-11-19 17:42:11 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen bg-gray-400'>
+      <h1 className='text-3xl font-bold text-gray-500'>This will become the DRW Helper!</h1>
+    </div>
+  );
+}
+
+export default App;
