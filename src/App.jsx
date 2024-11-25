423ace3922141fc626fe3aeafb1dd91499b167b0
Max <124299483+mzimmer90@users.noreply.github.com>
2024-11-25 15:37:11 +0000

    testing github page works

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes as Switch } from "react-router-dom";
-import ConstructionPage from "./pages/constructionPage/ConstructionPage";
 import "./App.css";
+import ConstructionPage from "./pages/constructionPage/ConstructionPage";
+import HelloWorld from "./pages/levelUpPage/HelloWorld";
 
 function App() {
   return (
     <Router basename='/DRW-helper'>
       <Switch>
         <Route path='/construction-planner' element={<ConstructionPage />} />
+        <Route path='/level-up-calc' element={<HelloWorld />} />
       </Switch>
     </Router>
   );
