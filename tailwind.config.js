0d5f5114c5289336428d1e57d6ae97db5a644b95
Max Zimmer <mzimmer90@gmail.com>
2024-12-18 21:24:42 +0000

    Added colour safelist to fix button toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "bg-yellow-100",
+    "bg-yellow-200",
+    "bg-yellow-400",
+    "bg-gray-400",
+    "bg-gray-600",
+    "bg-gray-700",
+  ],
   theme: {
     extend: {},
   },
